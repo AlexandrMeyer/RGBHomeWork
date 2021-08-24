f1b98a9d4bb45d9f49b49628578215233c978b52
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-08-24 14:44:25 +0300

    Finish correction

--- RGBHomeWork/ViewController.swift
+++ RGBHomeWork/ViewController.swift
@@ -22,8 +22,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupSliders()
-        changeLabetText()
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+        
+        setupColor()
     }
     
     override func viewWillLayoutSubviews() {
@@ -34,37 +38,41 @@ class ViewController: UIViewController {
     
     @IBAction func sliderAction(_ sender: UISlider) {
         
-        changeLabetText()
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenValueLabel.text = string(from: greenSlider)
+        default:
+            blueValueLabel.text = string(from: blueSlider)
+        }
         
-        let red = CGFloat(redSlider.value)
-        let green = CGFloat(greenSlider.value)
-        let blue = CGFloat(blueSlider.value)
-        
-        colorMixView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        setupColor()
     }
     
-    private func changeLabetText() {
-        redValueLabel.text = String(NSString(format:"%.2f", redSlider.value))
-        greenValueLabel.text = String(NSString(format:"%.2f", greenSlider.value))
-        blueValueLabel.text = String(NSString(format:"%.2f", blueSlider.value))
+    private func string(from slider: UISlider) -> String {
+        String(format:"%.2f", slider.value)
     }
     
-    private func setupSliders() {
-        redSlider.minimumValue = 0
-        greenSlider.minimumValue = 0
-        blueSlider.minimumValue = 0
-        
-        redSlider.maximumValue = 1
-        greenSlider.maximumValue = 1
-        blueSlider.maximumValue = 1
-        
-        redSlider.value = 1
-        greenSlider.value = 1
-        blueSlider.value = 1
-        
-        redSlider.minimumTrackTintColor = .red
-        greenSlider.minimumTrackTintColor = .green
-        blueSlider.minimumTrackTintColor = .blue
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                redValueLabel.text = string(from: redSlider)
+            case greenValueLabel:
+                greenValueLabel.text = string(from: greenSlider)
+            default:
+                blueValueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setupColor() {
+        colorMixView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
     }
 }
 
